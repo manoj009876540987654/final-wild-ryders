0775e5960e91cde191601f45492c07cc0fd75114
manoj009876540987654 <144827758+manoj009876540987654@users.noreply.github.com>
2025-11-13 21:53:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 window._config = {
     api: {
-        invokeUrl: 'https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod' // Replace with your API URL
+        invokeUrl: 'https://zsievbkdqg.execute-api.ap-south-1.amazonaws.com/sbi' // Replace with your API URL
     }
 };
+
 
